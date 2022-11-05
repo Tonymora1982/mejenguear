2ade0de36ee61584d2e4c25966fd5cc5f8977687
Anthony Mora P <tonymora1982@gmail.com>
2022-11-05 13:02:04 -0600

    Agregando NavBar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './images/balon.png';
 import './App.css';
 import Login from './components/Login';
+import NavBar from './components/NavBar';
 
 
 function App() {
   return (
     <div className="App">
-
+        <div>
+        < NavBar />
+        </div>
         <div>
         <img src={logo} className="App-logo" alt="logo" />
         </div>
